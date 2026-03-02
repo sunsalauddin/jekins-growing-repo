83eab3ff71ff41914f60a6eb6ad1e86ed7950968
salauddin <syadsalauddin04645@gmail.com>
2026-03-02 22:18:36 +0530

    compile successful

---
+++ basic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class WSCUBE {
+    public static void main(String[] args) {
+        
+        // Declare and assign an integer
+        int number = 18;
+        
+        // Print the integer value
+        System.out.println("The number is: " 
+                                + number);
+        System.out.println(2100);
+    }
+}
