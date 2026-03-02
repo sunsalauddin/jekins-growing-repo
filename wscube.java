9898e033d703b4709158c352d64bc84ac3fe871e
sunsalauddin <99324650+sunsalauddin@users.noreply.github.com>
2026-03-02 22:25:35 +0530

    file wscube.java

---
+++ wscube.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class WSCUBE {
+    public static void main(String[] args) {
+        
+        // Create Scanner object for input
+        Scanner sc = new Scanner(System.in);  
+        System.out.print("Enter an integer: ");
+        
+        // Read integer from user
+        int number = sc.nextInt();  
+        
+        // Print the entered number
+        System.out.println("You entered: " 
+                                + number);  
+    }
+}
